d0d4d340dae14903ac7f42ca8b399beb85a9a376
Jaime <jaime.chapinal@gmail.com>
2024-01-24 23:02:19 +0100

    Fixes twitter

--- src/twitterInject.js
+++ src/twitterInject.js
@@ -1,6 +1,5 @@
 // We need to inject the script in the twitter page
 // in order to be able to inspect all the XHR requests
-
 const element = document.createElement("script");
 element.src = chrome.runtime.getURL("src/twitterDownloader.js");
 (document.head || document.documentElement).appendChild(element);
